5713cfd5923bdeae14870774115c94ee748ef93e
natalie-r39 <nrulton@gmail.com>
2024-08-14 13:05:48 +1000

    enumerate

--- enumerate.py
+++ enumerate.py
@@ -16,13 +16,9 @@ for user in users_list:
         body = response.json()
         if vuln:  # Password Enumeration
             if user and request_data.get('password') != user.password:
-            return Response(error_message_helper("Password is not correct for the given username. Week2 Completed"), 200, mimetype="application/json")
-        elif not user:  # User enumeration
-            return Response(error_message_helper("Username does not exist"), 200, mimetype="application/json")
+                return Response(error_message_helper("Password is not correct for the given username. Week2 Completed"), 200, mimetype="application/json")
+            elif not user:  # User enumeration
+                return Response(error_message_helper("Username does not exist"), 200, mimetype="application/json")
         else:
             if (user and request_data.get('password') != user.password) or (not user):
-            return Response(error_message_helper("Username or Password Incorrect!"), 200, mimetype="application/json")
-        elif body["message"] == "Successfully logged in.":
-            print(f"Found {user} with password {password}")
-        else:
-            break
+                return Response(error_message_helper("Username or Password Incorrect!"), 200, mimetype="application/json")
